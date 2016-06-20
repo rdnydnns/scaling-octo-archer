433fb717c9274b57c0147c2dc0d2aa57870412b6
Rodney Dennis <rodneydennisjr@gmail.com>
2016-06-20 15:15:58 -0500

    finish up

---
+++ README.md
@@ -0,0 +1,3 @@
+# [scroll-o-nacci](http://http://rod.today/scroll-o-nacci)
+
+> 100 Days of Code: Day 5
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('\'Allo \'Allo!');
+const form = document.body.querySelector('form');
+const container = document.body.querySelector('#container');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  container.innerHTML = '';
+  const num = document.body.querySelector('#num').value;
+  fib(num);
+});
+
+function fib(num) {
+  // Init Fibonacci Sequence
+  const fseq = [0, 1];
+
+  for(var i = 2; i < num; i++) {
+    const f = fseq[i - 1] + fseq[i - 2];
+    fseq.push(f);
+  }
+
+  fseq.forEach((val, i, fseq) => {
+    const marker = document.createElement('label');
+    marker.style.height = `${val}rem`;
+    marker.style.zIndex = `-${val}`;
+    marker.innerHTML = `<span>F${i+1}</span><span>${val}</span>`;
+    container.appendChild(marker);
+  });
+}
